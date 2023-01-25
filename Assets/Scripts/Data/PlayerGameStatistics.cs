0053e158b797ccb29b857dd1ad87aa902c31ce23
Volodymyr Fedynjak <85686566+Volplayed@users.noreply.github.com>
2023-01-25 20:12:06 +0200

    added functions to change values in playergamestatistics.cs

--- Assets/Scripts/Data/PlayerGameStatistics.cs
+++ Assets/Scripts/Data/PlayerGameStatistics.cs
@@ -91,5 +91,269 @@ public class PlayerGameStatistics : MonoBehaviour
     public static int movesMadePlayer3 = 0;
     public static int movesMadePlayer4 = 0;
     
+    //functions
+    public static void addGamesPlayed() {
+        gamesPlayed++;
+    }
+
+    //add games won
+    public static void addGamesWon(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                gamesWonPlayer1++;
+                break;
+            case 2:
+                gamesWonPlayer2++;
+                break;
+            case 3:
+                gamesWonPlayer3++;
+                break;
+            case 4:
+                gamesWonPlayer4++;
+                break;
+        }
+    }
+
+    //add games lost
+    public static void addGamesLost(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                gamesLostPlayer1++;
+                break;
+            case 2:
+                gamesLostPlayer2++;
+                break;
+            case 3:
+                gamesLostPlayer3++;
+                break;
+            case 4:
+                gamesLostPlayer4++;
+                break;
+        }
+    }
+
+    //add mobs killed
+    public static void addMobsKilled(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                mobsKilledPlayer1++;
+                break;
+            case 2:
+                mobsKilledPlayer2++;
+                break;
+            case 3:
+                mobsKilledPlayer3++;
+                break;
+            case 4:
+                mobsKilledPlayer4++;
+                break;
+        }
+    }
+
+    //add players killed
+    public static void addPlayersKilled(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                playersKilledPlayer1++;
+                break;
+            case 2:
+                playersKilledPlayer2++;
+                break;
+            case 3:
+                playersKilledPlayer3++;
+                break;
+            case 4:
+                playersKilledPlayer4++;
+                break;
+        }
+    }
+
+    //add deaths
+    public static void addDeaths(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                deathsPlayer1++;
+                break;
+            case 2:
+                deathsPlayer2++;
+                break;
+            case 3:
+                deathsPlayer3++;
+                break;
+            case 4:
+                deathsPlayer4++;
+                break;
+        }
+    }
+
+    //add damage dealt
+    public static void addDamageDealt(int playerNumber, int damage) {
+        switch (playerNumber) {
+            case 1:
+                damageDealtPlayer1 += damage;
+                break;
+            case 2:
+                damageDealtPlayer2 += damage;
+                break;
+            case 3:
+                damageDealtPlayer3 += damage;
+                break;
+            case 4:
+                damageDealtPlayer4 += damage;
+                break;
+        }
+    }
+
+    //add damage received
+    public static void addDamageReceived(int playerNumber, int damage) {
+        switch (playerNumber) {
+            case 1:
+                damageReceivedPlayer1 += damage;
+                break;
+            case 2:
+                damageReceivedPlayer2 += damage;
+                break;
+            case 3:
+                damageReceivedPlayer3 += damage;
+                break;
+            case 4:
+                damageReceivedPlayer4 += damage;
+                break;
+        }
+    }
+
+    //add healing done
+    public static void addHealingDone(int playerNumber, int healing) {
+        switch (playerNumber) {
+            case 1:
+                healingDonePlayer1 += healing;
+                break;
+            case 2:
+                healingDonePlayer2 += healing;
+                break;
+            case 3:
+                healingDonePlayer3 += healing;
+                break;
+            case 4:
+                healingDonePlayer4 += healing;
+                break;
+        }
+    }
+
+    //add fights escaped
+    public static void addFightsEscaped(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                fightsEscapedPlayer1++;
+                break;
+            case 2:
+                fightsEscapedPlayer2++;
+                break;
+            case 3:
+                fightsEscapedPlayer3++;
+                break;
+            case 4:
+                fightsEscapedPlayer4++;
+                break;
+        }
+    }
+
+    //add levels gained
+    public static void addLevelsGained(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                levelsGainedPlayer1++;
+                break;
+            case 2:
+                levelsGainedPlayer2++;
+                break;
+            case 3:
+                levelsGainedPlayer3++;
+                break;
+            case 4:
+                levelsGainedPlayer4++;
+                break;
+        }
+    }
+
+    //check max level reached
+    public static void checkMaxLevelReached(int playerNumber, int level) {
+        switch (playerNumber) {
+            case 1:
+                if (level > maxLevelReachedPlayer1) {
+                    maxLevelReachedPlayer1 = level;
+                }
+                break;
+            case 2:
+                if (level > maxLevelReachedPlayer2) {
+                    maxLevelReachedPlayer2 = level;
+                }
+                break;
+            case 3:
+                if (level > maxLevelReachedPlayer3) {
+                    maxLevelReachedPlayer3 = level;
+                }
+                break;
+            case 4:
+                if (level > maxLevelReachedPlayer4) {
+                    maxLevelReachedPlayer4 = level;
+                }
+                break;
+        }
+    }
+
+    //add items equipped
+    public static void addItemsEquipped(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                itemsEquippedPlayer1++;
+                break;
+            case 2:
+                itemsEquippedPlayer2++;
+                break;
+            case 3:
+                itemsEquippedPlayer3++;
+                break;
+            case 4:
+                itemsEquippedPlayer4++;
+                break;
+        }
+    }
+
+    //add items declined
+    public static void addItemsDeclined(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                itemsDeclinedPlayer1++;
+                break;
+            case 2:
+                itemsDeclinedPlayer2++;
+                break;
+            case 3:
+                itemsDeclinedPlayer3++;
+                break;
+            case 4:
+                itemsDeclinedPlayer4++;
+                break;
+        }
+    }
+
+    //add moves made
+    public static void addMovesMade(int playerNumber) {
+        switch (playerNumber) {
+            case 1:
+                movesMadePlayer1++;
+                break;
+            case 2:
+                movesMadePlayer2++;
+                break;
+            case 3:
+                movesMadePlayer3++;
+                break;
+            case 4:
+                movesMadePlayer4++;
+                break;
+        }
+    }
 
 }
