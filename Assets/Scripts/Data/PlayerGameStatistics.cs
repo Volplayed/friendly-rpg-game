44a24de916aab8e84be571030609abd9c8ac0912
Volodymyr Fedynjak <85686566+Volplayed@users.noreply.github.com>
2023-01-25 20:05:12 +0200

    added playerGameStatistics.cs with game stats

---
+++ Assets/Scripts/Data/PlayerGameStatistics.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGameStatistics : MonoBehaviour
+{
+    //games played
+    public static int gamesPlayed = 0;
+
+    //games won
+    public static int gamesWonPlayer1 = 0;
+    public static int gamesWonPlayer2 = 0;
+    public static int gamesWonPlayer3 = 0;
+    public static int gamesWonPlayer4 = 0;
+
+    //games lost
+    public static int gamesLostPlayer1 = 0;
+    public static int gamesLostPlayer2 = 0;
+    public static int gamesLostPlayer3 = 0;
+    public static int gamesLostPlayer4 = 0;
+
+    //mobs killed
+    public static int mobsKilledPlayer1 = 0;
+    public static int mobsKilledPlayer2 = 0;
+    public static int mobsKilledPlayer3 = 0;
+    public static int mobsKilledPlayer4 = 0;
+
+    //players killed
+    public static int playersKilledPlayer1 = 0;
+    public static int playersKilledPlayer2 = 0;
+    public static int playersKilledPlayer3 = 0;
+    public static int playersKilledPlayer4 = 0;
+
+    //deaths
+    public static int deathsPlayer1 = 0;
+    public static int deathsPlayer2 = 0;
+    public static int deathsPlayer3 = 0;
+    public static int deathsPlayer4 = 0;
+    
+    //damage dealt
+    public static int damageDealtPlayer1 = 0;
+    public static int damageDealtPlayer2 = 0;
+    public static int damageDealtPlayer3 = 0;
+    public static int damageDealtPlayer4 = 0;
+
+    //damage received
+    public static int damageReceivedPlayer1 = 0;
+    public static int damageReceivedPlayer2 = 0;
+    public static int damageReceivedPlayer3 = 0;
+    public static int damageReceivedPlayer4 = 0;
+
+    //healing done
+    public static int healingDonePlayer1 = 0;
+    public static int healingDonePlayer2 = 0;
+    public static int healingDonePlayer3 = 0;
+    public static int healingDonePlayer4 = 0;
+
+    //fights escaped
+    public static int fightsEscapedPlayer1 = 0;
+    public static int fightsEscapedPlayer2 = 0;
+    public static int fightsEscapedPlayer3 = 0;
+    public static int fightsEscapedPlayer4 = 0;
+
+    //levels gained
+    public static int levelsGainedPlayer1 = 0;
+    public static int levelsGainedPlayer2 = 0;
+    public static int levelsGainedPlayer3 = 0;
+    public static int levelsGainedPlayer4 = 0;
+
+    //max level reached
+    public static int maxLevelReachedPlayer1 = 1;
+    public static int maxLevelReachedPlayer2 = 1;
+    public static int maxLevelReachedPlayer3 = 1;
+    public static int maxLevelReachedPlayer4 = 1;
+
+    //items equipped
+    public static int itemsEquippedPlayer1 = 0;
+    public static int itemsEquippedPlayer2 = 0;
+    public static int itemsEquippedPlayer3 = 0;
+    public static int itemsEquippedPlayer4 = 0;
+
+    //items declined
+    public static int itemsDeclinedPlayer1 = 0;
+    public static int itemsDeclinedPlayer2 = 0;
+    public static int itemsDeclinedPlayer3 = 0;
+    public static int itemsDeclinedPlayer4 = 0;
+
+    //moves made
+    public static int movesMadePlayer1 = 0;
+    public static int movesMadePlayer2 = 0;
+    public static int movesMadePlayer3 = 0;
+    public static int movesMadePlayer4 = 0;
+    
+
+}
